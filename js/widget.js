cdd4c42d1dc4d8c00f0a63275ddb24f22bc97e58
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-07 02:33:46 +0530

    callback skelton

--- js/widget.js
+++ js/widget.js
@@ -1,2 +1,9 @@
 // Create a new XML Http Request (XHR) Object
 var xhr = new XMLHttpRequest();
+
+// Skelton of a callback function
+xhr.onreadystatechange = function() {
+  if(xhr.readyState ===4){
+
+  }
+}
